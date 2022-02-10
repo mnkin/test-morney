ab0ea24a8fe6d36972b53869f2e5ba900d9e7b19
mnkin <2664306866@qq.com>
2022-02-10 16:02:09 +0800

    添加404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import VueRouter, { RouteConfig } from 'vue-router'
 import Money from "@/views/Money.vue"
 import Labels from "@/views/Labels.vue"
 import Statistics from "@/views/Statistics.vue"
+import NotFound from "@/views/NotFound.vue"
 
 Vue.use(VueRouter)
 
@@ -22,7 +23,11 @@ const routes: Array<RouteConfig> = [
   {
     path: "/statistics",
     component: Statistics
-  }
+  },
+  {
+    path: '*',
+    component: NotFound
+  },
   // {
   //   path: '/',
   //   name: 'Home',
---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>当前页面不存在，请检查网址是否输入正确</div>
+    <div>
+      <router-link to="/">返回首页</router-link>
+      <!-- <a href="#/">返回首页</a> -->
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style scoped>
+</style>
