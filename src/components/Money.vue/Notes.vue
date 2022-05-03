39c515847c72c8c37283a038f74425feb1fc0399
mnkin <2664306866@qq.com>
2022-05-03 15:54:46 +0800

    基本完成NumberPad组件

--- src/components/Money.vue/Notes.vue
+++ src/components/Money.vue/Notes.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
     <div>
+<!--      {{content}}-->
       <label class="notes">
-        <span class="name">备注</span>
-        <input type="text" placeholder="在这里输入备注" />
+        <span class="name" >备注</span>
+        <input type="text" placeholder="在这里输入备注"/>
       </label>
     </div>
   </div>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import Vue from 'vue'
+import {Component} from "vue-property-decorator";
+
+@Component
+export default class Notes extends Vue {
+  // content:String = ''
+}
 </script>
 
 <style lang="scss" scoped>
@@ -18,9 +25,11 @@ export default {};
   background: #f5f5f5;
   display: flex;
   align-items: center;
+
   > .name {
     padding: 0px 16px;
   }
+
   > input {
     height: 64px;
     flex-grow: 1;
