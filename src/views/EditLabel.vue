af911b35c596727efc8124e2bd7dfc6fbd67a805
mnkin <2664306866@qq.com>
2022-05-21 15:48:57 +0800

    将tag类型由string改为对象{id:string,name:string}

---
+++ src/views/EditLabel.vue
@@ -0,0 +1,19 @@
+<template>
+  <Layout>
+    编辑页面
+  </Layout>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class EditLabel extends Vue {
+  name: 'EditLabel';
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
